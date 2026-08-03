ceac79e61b5324ea7a916e4771abebeed0076bc4
unknown <petroshambirr@gmail.com>
2026-08-03 16:13:52 +0300

    fix nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,38 @@
 
 
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Welcome from './components/Welcome'; // ሓድሽ ኮምፖነንትካ
+// import Gallery from './components/Gallery'; // ኣብዚ ኮምፖነንት ኣሎ
+// import AdminLogin from './components/AdminLogin'; // ኣብዚ ኮምፖነንት ኣሎ
+// import AdminDashboard from './components/AdminDashboard'; // ኣብዚ ኮምፖነንት ኣሎ
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         {/* መጀመርታ ናብ ዌልካም ቪዲዮ ይኣትዉ */}
+//         <Route path="/" element={<Welcome />} />
+        
+//         {/* ምስ ተወድአ ወይ ክሊክ ምስ ገበሩ ናብ Home ይሓልፉ */}
+//         <Route path="/home" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/gallery/:category" element={<Gallery />} /> 
+//         <Route path="/admin-login" element={<AdminLogin />} />
+//         <Route path="/admin-panel" element={<AdminDashboard />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Price from './pages/Price';               // ሓድሽ: ናይ Price ኮምፖነንት
+import Contact from './pages/Contact';           // ሓድሽ: ናይ Contact ኮምፖነንት
 import Welcome from './components/Welcome'; // ሓድሽ ኮምፖነንትካ
 import Gallery from './components/Gallery'; // ኣብዚ ኮምፖነንት ኣሎ
 import AdminLogin from './components/AdminLogin'; // ኣብዚ ኮምፖነንት ኣሎ
@@ -18,6 +48,8 @@ function App() {
         {/* ምስ ተወድአ ወይ ክሊክ ምስ ገበሩ ናብ Home ይሓልፉ */}
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/price" element={<Price />} />         {/* ሓድሽ Route */}
+        <Route path="/contact" element={<Contact />} />     {/* ሓድሽ Route */}
         <Route path="/gallery/:category" element={<Gallery />} /> 
         <Route path="/admin-login" element={<AdminLogin />} />
         <Route path="/admin-panel" element={<AdminDashboard />} />
