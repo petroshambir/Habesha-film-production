6880b6b8b56b1293d64ca08673be66f203a9ab8c
unknown <petroshambirr@gmail.com>
2026-08-07 19:13:47 +0300

    viw to gallery

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -61,10 +61,45 @@
 // }
 
 // export default App;
+
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Price from './pages/Price';             
+// import Contact from './pages/Contact';         
+// import Welcome from './components/Welcome'; 
+// import Gallery from './components/Gallery'; 
+// import AdminLogin from './components/AdminLogin'; 
+// import AdminDashboard from './components/AdminDashboard'; 
+// import ClientSelection from './components/ClientSelection';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Welcome />} />
+//         <Route path="/home" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/price" element={<Price />} />         
+//         <Route path="/contact" element={<Contact />} />     
+        
+//         {/* ሓደ ኮይኑ ንኩሉ ዓይነት ጋለሪ ብቑዕ ዝዀነ Route */}
+//         <Route path="/gallery/:category" element={<Gallery />} /> 
+
+//         <Route path="/admin-login" element={<AdminLogin />} />
+//         <Route path="/admin-panel" element={<AdminDashboard />} />
+//         <Route path="/client-selection" element={<ClientSelection />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Price from './pages/Price';             
+import Price from './pages/Price';              
 import Contact from './pages/Contact';         
 import Welcome from './components/Welcome'; 
 import Gallery from './components/Gallery'; 
@@ -79,8 +114,8 @@ function App() {
         <Route path="/" element={<Welcome />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/price" element={<Price />} />         
-        <Route path="/contact" element={<Contact />} />     
+        <Route path="/price" element={<Price />} />        
+        <Route path="/contact" element={<Contact />} />    
         
         {/* ሓደ ኮይኑ ንኩሉ ዓይነት ጋለሪ ብቑዕ ዝዀነ Route */}
         <Route path="/gallery/:category" element={<Gallery />} /> 
