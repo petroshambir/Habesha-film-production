928f9dbd755cd40cf202fd2e11c38c1328742394
unknown <petroshambirr@gmail.com>
2026-08-12 12:57:30 +0300

    fix server

--- server/models/ClientProject.js
+++ server/models/ClientProject.js
@@ -14,14 +14,34 @@
 // export default ClientProject;
 
 
+// import mongoose from 'mongoose';
+
+// const clientProjectSchema = new mongoose.Schema({
+//     clientName: { type: String, required: true },
+//     portalNumber: { type: String, required: true },
+//     passcode: { type: String, required: true },
+//     images: [{ type: String }],
+//     selectedImages: [{ type: String }],
+//     isCompleted: { type: Boolean, default: false }
+// }, { timestamps: true });
+
+// const ClientProject = mongoose.model('ClientProject', clientProjectSchema);
+// export default ClientProject;
+
 import mongoose from 'mongoose';
 
 const clientProjectSchema = new mongoose.Schema({
     clientName: { type: String, required: true },
     portalNumber: { type: String, required: true },
     passcode: { type: String, required: true },
-    images: [{ type: String }],
-    selectedImages: [{ type: String }],
+    images: [{
+        original: { type: String, required: true },
+        compressed: { type: String, required: true }
+    }],
+    selectedImages: [{
+        original: { type: String, required: true },
+        compressed: { type: String, required: true }
+    }],
     isCompleted: { type: Boolean, default: false }
 }, { timestamps: true });
 
