1742a3d1b824dd18d8586580126dffb18c892740
unknown <petroshambirr@gmail.com>
2026-08-15 16:24:23 +0300

    fix contact

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -337,6 +337,8 @@ export default function Contact() {
                 <a href="tel:+251976130175" className="text-zinc-400 text-sm mt-1 font-light hover:text-[#dfb557] transition-colors underline underline-offset-4">
                   +251 976130175
                 </a>
+                <br />
+                <br />
                 <a href="tel:+251942746150" className="text-zinc-400 text-sm mt-1 font-light hover:text-[#dfb557] transition-colors underline underline-offset-4">
                   +251 942746150
                 </a>
